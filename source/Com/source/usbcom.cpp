8aebd588cc3189113da88ec2aab5960748542730
wanglnet <wanglnet@outlook.com>
2016-07-07 11:54:36 +0800

    add for usb hub

--- source/Com/source/usbcom.cpp
+++ source/Com/source/usbcom.cpp
@@ -2,9 +2,12 @@
 #include <QDir>
 #include <QStringList>
 #include <QDebug>
-
-//ttyUSB0 -> ../../devices/ff540000.usb/usb3/3-1/3-1.1/3-1.1:1.0/ttyUSB0/tty/ttyUSB0
-//ttyUSB1 -> ../../devices/ff540000.usb/usb3/3-1/3-1.2/3-1.2:1.0/ttyUSB1/tty/ttyUSB1
+//board usb
+//ttyUSB0 -> ../../devices/platform/bcm2708_usb/usb1/1-1/1-1.1/1-1.1:1.0/ttyUSB0/tty/ttyUSB0
+//ttyUSB0 -> ../../devices/platform/bcm2708_usb/usb1/1-1/1-1.2/1-1.2:1.0/ttyUSB1/tty/ttyUSB1
+//usb hub
+//ttyUSB0 -> ../../devices/platform/bcm2708_usb/usb1/1-1/1-1.2/1-1.2.3/1-1.2.3:1.0/ttyUSB0/tty/ttyUSB0
+//ttyUSB0 -> ../../devices/platform/bcm2708_usb/usb1/1-1/1-1.2/1-1.2.4/1-1.2.4:1.0/ttyUSB1/tty/ttyUSB1
 QString UsbCom_Def2Real(QString strDef)
 {
 	QDir dir("/sys/class/tty");
@@ -22,7 +25,7 @@ QString UsbCom_Def2Real(QString strDef)
 		
 		if(nPart < 6)	continue;
 		QString strID = strLinkPart.at(nPart - 5);
-		strID = strID.section('.', 1, 1);
+		strID = strID.section('.', 1);
 		printf("strID : %s\n", strID.toLocal8Bit().data());
 		if(strDef.compare(QString("%1%2").arg(USBCOM_PREFIX).arg(strID), Qt::CaseInsensitive) == 0)
 		{
@@ -37,6 +40,11 @@ QString UsbCom_Def2Real(QString strDef)
 QStringList UsbComList()
 {
 	QStringList list;
-	list << "UsbCom2" << "UsbCom3" << "UsbCom4" << "UsbCom5";
+	list << "UsbCom2" << "UsbCom3" << "UsbCom4" << "UsbCom5"
+		 << "UsbCom2.1" << "UsbCom2.2" << "UsbCom2.3" << "UsbCom2.4"
+		 << "UsbCom3.1" << "UsbCom3.2" << "UsbCom3.3" << "UsbCom3.4"
+		 << "UsbCom4.1" << "UsbCom4.2" << "UsbCom4.3" << "UsbCom4.4"
+		 << "UsbCom5.1" << "UsbCom5.2" << "UsbCom5.3" << "UsbCom5.4"
+		 ;
 	return list;
 }
